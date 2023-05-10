193cebd4a7388199d144b51c581f05d97394a5e0
AbazMax <maxim.abazov@gmail.com>
2023-05-10 15:12:16 +0300

    model created

--- create_pdf_cert/admin.py
+++ create_pdf_cert/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Cert
 
-# Register your models here.
+@admin.register(Cert)
+class CertAdmin(admin.ModelAdmin):
+    list_filter = ('date', 'name', 'course',)
+    list_display = ('name', 'course', 'date',)
--- create_pdf_cert/models.py
+++ create_pdf_cert/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Cert(models.Model):
+    name = models.CharField(max_length=50)
+    course = models.CharField(max_length=50)
+    date = models.DateField(auto_now=True)
+    cert = models.FileField(upload_to=('create_pdf_cert/certificates/'))
+
+    class Meta:
+        ordering = ('date',)
+        verbose_name_plural = 'Certificates'
+        
+    def __str__(self):
+        return f'{self.id}. {self.name}, {self.date}'
+    
